a08f6441a47bad188fd5db0d85bb616ba5f4fbe9
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-01-11 17:33:44 +0800

    сообщения

---
+++ database/migrations/2024_01_11_084254_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('chat_id')->nullable();
+            $table->bigInteger('from_id')->nullable();
+            $table->bigInteger('id_to')->default(0);
+            $table->string('text')->nullable();
+            $table->boolean('is_delivered')->default(0);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,1 +1,28 @@
-Страничка чата
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Чаты
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                @if (isset($message) && count($message))
+                    @foreach($message as $m)
+                        <div class="p-6 text-gray-900 dark:text-gray-100" @if($m->from_id === \Illuminate\Support\Facades\Auth::user()->id)  @endif>
+                            текст сообщения 1
+                        </div>
+                    @endforeach
+                @else
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        в этом чате нет сообщений
+                    </div>
+                @endif
+
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
+
