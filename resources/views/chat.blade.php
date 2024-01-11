ec61113d7855cdbe8aac24c5846aea245c5ef21b
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2024-01-11 17:38:04 +0800

    немного исправил чат

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,3 +1,6 @@
+<?php
+$userId = \Illuminate\Support\Facades\Auth::user()->id;
+?>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -10,7 +13,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 @if (isset($message) && count($message))
                     @foreach($message as $m)
-                        <div class="p-6 text-gray-900 dark:text-gray-100" @if($m->from_id === \Illuminate\Support\Facades\Auth::user()->id)  @endif>
+                        <div class="p-6 text-gray-900 dark:text-gray-100" @if($m->from_id === $userId)  @endif>
                             текст сообщения 1
                         </div>
                     @endforeach
